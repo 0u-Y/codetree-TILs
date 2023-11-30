9a98abed98b038e02bfa652a11c9d10e8cfa59d7
YoungWoo Yang <119781151+0u-Y@users.noreply.github.com>
2023-11-30 22:21:16 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231130/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 231130/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -4,54 +4,44 @@
 
 using namespace std;
 
-int dx[2] = {1,0};
-int dy[2] = {0,-1};
-
+int dx[2] = {1, 0};
+int dy[2] = {0, -1};
 
 bool vis[101][101] = {0};
 
-
-
-
-
-int n,m;
+int n, m;
 int map[101][101];
 
-
-void DFS(int a, int b){
-    for(int i=0; i<2; i++){
+void DFS(int a, int b) {
+    for (int i = 0; i < 2; i++) {
         int nx = a + dx[i];
         int ny = b + dy[i];
-        if(nx >=m || nx < 0 || ny >= n || ny < 0) continue;
-        if(map[nx][ny] == 0 || vis[nx][ny] == 1) continue;
-        if(nx == m-1 && ny == n-1){
-            cout<<1;
-            return ;
+        if (nx >= m || nx < 0 || ny >= n || ny < 0) continue;
+        if (map[nx][ny] == 0 || vis[nx][ny] == 1) continue;
+        if (nx == m - 1 && ny == n - 1) {
+            cout << 1 << endl;
+            return;
         }
         vis[nx][ny] = 1;
-        DFS(nx,ny);
+        DFS(nx, ny);
     }
-    
 }
 
-
-
 int main() {
+    cin >> m >> n;
 
-    for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
-            cin>>map[i][j];
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> map[i][j];
         }
     }
 
     vis[0][0] = 1;
-    DFS(0,0);
+    DFS(0, 0);
 
-    if(vis[m-1][n-1] == 0){
-        cout<<0;
+    if (vis[m - 1][n - 1] == 0) {
+        cout << 0 << endl;
     }
 
-    
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
