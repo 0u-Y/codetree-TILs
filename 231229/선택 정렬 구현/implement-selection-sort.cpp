3cd2261592167e5a08411ccbd8f83cf08327d212
YoungWoo Yang <119781151+0u-Y@users.noreply.github.com>
2023-12-29 10:02:27 +0900

    Title: 선택 정렬 구현, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231229/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    int min;
+    int arr[101];
+
+    int temp;
+
+    for(int i=0; i<n; i++){
+        int num;
+        cin>>num;
+        arr[i] = num;
+    }
+
+    for(int i=0; i<n-1; i++){
+        min = i;
+        for(int j= i+1; j<n; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+                temp = arr[j];
+                arr[j] = arr[min];
+                arr[min] = temp;
+
+            }
+        }
+
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
