2bd16e59a9dca8c07dee59c134c1eb697529e938
YoungWoo Yang <119781151+0u-Y@users.noreply.github.com>
2024-01-22 10:10:02 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240122/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240122/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,96 +1,56 @@
 #include <iostream>
-
-
 using namespace std;
 
-int n,m;
-
-
+int n, m;
 int ans;
-
 int map[101][101];
 
-
-void checkRow(int y){
-    if(m == 1){
-        ans++;
-        return;
-    }
+void checkColumn(int y) {
     int count = 1;
-
-    for(int i=0; i<n; i++){
-        if(map[i][y] == map[i+1][y]){
+    for (int i = 0; i < n - 1; i++) {
+        if (map[i][y] == map[i + 1][y]) {
             count++;
-            if(count >= m){
+            if (count >= m) {
                 ans++;
                 break;
             }
-            else{
-                count = 1;
-            }
+        } else {
+            count = 1;
         }
-
     }
-    if(n == 1 && m == 1){
-        ans++;
-    }
-
+    if (n == 1 && m == 1) ans++;
 }
 
-void checkColumn(int x){
-    if(m == 1){
-        ans++;
-        return;
-    }
+void checkRow(int x) {
     int count = 1;
-
-    for(int i=0; i<n-1; i++){
-        if(map[x][i] == map[x][i+1]){
+    for (int i = 0; i < n - 1; i++) {
+        if (map[x][i] == map[x][i + 1]) {
             count++;
-            if(count >= m){
+            if (count >= m) {
                 ans++;
                 break;
             }
-            else{
-                count = 1;
-            }
+        } else {
+            count = 1;
         }
     }
-    
-    if(n == 1 && m == 1){
-        ans++;
-    }
-
-
+    if (n == 1 && m == 1) ans++;
 }
-
-
-
-
-
-
 
 int main() {
-    cin>>n>>m;
-
+    cin >> n >> m;
     ans = 0;
 
-
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin>>map[j][i];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> map[j][i];
         }
     }
 
-    
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++) {
         checkRow(i);
         checkColumn(i);
     }
 
-
-    cout<<ans;
-
-
+    cout << ans;
 }
