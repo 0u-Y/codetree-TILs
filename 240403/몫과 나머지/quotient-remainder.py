0f226269c8d3d68b904ae51c4b7a8ae898adca52
YoungWoo Yang <119781151+0u-Y@users.noreply.github.com>
2024-04-03 17:44:33 +0900

    Title: 몫과 나머지, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240403/몫과 나머지/quotient-remainder.py
@@ -0,0 +1,14 @@
+a,b = map(int,input().split())
+
+
+
+if(a > b):
+    m = a/b
+    n = a%b
+    print(str(m) + "..." + str(n))
+    exit()
+else:
+    m = b/a
+    n = b%a
+    print(str(n) + "..." + str(m))
+    exit()
